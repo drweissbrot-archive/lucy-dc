4f20a81c241020b4024ea0171477826e32d5a68c
Pierre Borgmann <drweissbrot@outlook.com>
2017-02-08 23:29:24 +0100

    typos <3

--- Lucy/Services.js
+++ Lucy/Services.js
@@ -111,7 +111,7 @@ class Services {
 
 		if (isFluttertrain && ! this.cache.fluttertrainPosted) {
 			return this.broadcast('https://orig05.deviantart.net/b7b5/f/2013/268/8/0/fluttertrain_by_bronycopter-d6ntenh.gif')
-		})
+		}
 	}
 }
 
